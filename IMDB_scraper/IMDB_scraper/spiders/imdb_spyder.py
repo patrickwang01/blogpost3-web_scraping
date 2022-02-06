72c46ba34a8f50024569e6a55956943850cc2eb1
Patrick Wang <wangpatrick2001@gmail.com>
2022-02-05 19:28:39 -0800

    updates

--- IMDB_scraper/IMDB_scraper/spiders/imdb_spyder.py
+++ IMDB_scraper/IMDB_scraper/spiders/imdb_spyder.py
@@ -2,6 +2,8 @@ import scrapy
 from scrapy.spiders import Spider
 from scrapy.http import Request
 
+# C:/Users/wangp/anaconda3/Scripts/activate
+
 class ImdbSpider(scrapy.Spider):
     name = 'imdb_spider'
 
@@ -9,9 +11,41 @@ class ImdbSpider(scrapy.Spider):
 
     
     def parse(self, response):
+        
+        # get href portion of 'cast and crew' item
+        cast = response.css("li.ipc-inline-list__item a")[2].attrib["href"]
 
-        cast_crew = response.css("li.ipc-inline-list__item a").attrib["href"]
+        # join href with main url 
+        cast_crew = response.urljoin(cast)
 
         yield scrapy.Request(cast_crew, callback = self.parse_full_credits)
 
     
+    def parse_full_credits(self, response):
+        
+        # get href portion for each actor thumbnail
+        actor_names = [a.attrib["href"] for a in response.css("td.primary_photo a")]
+
+        # set main website link
+        main = "https://imdb.com"
+
+        # combine main website url with actor href 
+        actor_links = [main + actor for actor in actor_names]
+
+        # loop through entire list of actor urls
+        for link in actor_links:
+            yield scrapy.Request(link, callback = self.parse_actor_page)
+        
+    
+    def parse_actor_page(self, response):
+        
+        actor_name = response.css("span.itemprop::text").get() 
+ 
+        for row in response.css("div.filmo-row"):
+            
+            movie_or_TV_name = response.css("b a::text")
+
+            yield {
+                "actor": actor_name,
+                "movie_or_TV_name": movie_or_TV_name
+            }
